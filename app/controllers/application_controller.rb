4567d2159e013291f60f03065e1556fa4e5a9ee3
IssahBen <benedictissah@gmail.com>
2024-05-25 10:47:32 -0500

    first name ,roles and last name added to devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,6 @@
 class ApplicationController < ActionController::API
     acts_as_token_authentication_handler_for User, fallback: :none
+    def configure_permitted_parameters
+        devise_parameter_sanitizer.permit(:sign_up, keys: %i[role first_name last_name])
+      end
 end
